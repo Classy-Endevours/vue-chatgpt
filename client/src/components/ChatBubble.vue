9a1e134bb01ae6322b6e08f8785b7a3001c205ff
Piyush Dubey <piyush115dubey@gmail.com>
2023-06-23 18:15:30 +0530

    added chat design and component

---
+++ client/src/components/ChatBubble.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="chat-bubble" :class="{ 'user': isUserMessage }">
+    <div class="message">{{ message }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String,
+    isUserMessage: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+.chat-bubble {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 8px;
+  background-color: #f1f1f1;
+}
+
+.chat-bubble.user {
+  justify-content: flex-end;
+  background-color: #007bff;
+  color: #fff;
+}
+
+.message {
+  font-size: 14px;
+  line-height: 1.4;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,107 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div id="app">
+    <div class="chat-window">
+      <div class="chat-log">
+        <ChatBubble
+          v-for="message in messages"
+          :key="message.id"
+          :message="message.text"
+          :isUserMessage="message.isUserMessage"
+        />
+      </div>
+      <div class="input-container">
+        <input
+          v-model="newMessage"
+          @keyup.enter="sendMessage"
+          placeholder="Type your message..."
+          autofocus
+        />
+        <button @click="sendMessage">Send</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChatBubble from '../components/ChatBubble.vue';
+
+export default {
+  components: {
+    ChatBubble,
+  },
+  data() {
+    return {
+      messages: [],
+      newMessage: '',
+    };
+  },
+  methods: {
+    sendMessage() {
+      const messageText = this.newMessage.trim();
+      if (messageText !== '') {
+        this.messages.push({
+          id: this.messages.length,
+          text: messageText,
+          isUserMessage: true,
+        });
+        this.newMessage = '';
+        this.replyWithGPT(messageText);
+      }
+    },
+    replyWithGPT(messageText) {
+      // Perform API call or GPT processing to generate a reply
+      // For simplicity, we'll use a dummy response here
+      setTimeout(() => {
+        const replyText = `You said: "${messageText}". GPT-generated reply.`;
+        this.messages.push({
+          id: this.messages.length,
+          text: replyText,
+          isUserMessage: false,
+        });
+      }, 500);
+    },
+  },
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.chat-window {
+  max-width: 400px;
+  width: 100%;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.chat-log {
+  margin-bottom: 20px;
+}
+
+.input-container {
+  display: flex;
+}
+
+input {
+  flex: 1;
+  padding: 8px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  margin-right: 10px;
+}
+
+button {
+  padding: 8px 12px;
+  border: none;
+  border-radius: 4px;
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
